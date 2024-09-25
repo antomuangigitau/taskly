43b9e13f1c6c51a52b1ad4c65ffdedacc418e633
Antony <gitaumuangi@gmail.com>
2024-09-25 10:44:09 +0300

    Add a delete button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Pressable,
+  Alert,
+} from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Deleting..."),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+  };
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity style={styles.Button} onPress={handleDelete}>
+          <Text style={styles.textColor}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -17,14 +45,29 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
   },
+  // in react native every item has a flex property and the default direction is column
   itemContainer: {
     borderBottomColor: theme.colorCerulean,
     borderBottomWidth: 1,
     paddingHorizontal: 8,
     paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   itemText: {
     fontSize: 18,
     fontWeight: "200",
+  },
+  Button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 4,
+  },
+  textColor: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
