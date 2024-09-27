305154105f95c88f6e0efc3efc41c5cc70ae885a
Antony <gitaumuangi@gmail.com>
2024-09-27 08:52:28 +0300

    Set up bottom tabs navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,10 @@
 import { View, StyleSheet } from "react-native";
 import ShoppingListItem from "../components/ShoppingListItem";
 import { theme } from "../theme";
-import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
-      >
-        Go to Counter
-      </Link>
       <ShoppingListItem title="Coffee" />
       <ShoppingListItem title="Tea" isCompleted={true} />
       <ShoppingListItem title="Milk" isCompleted={true} />
