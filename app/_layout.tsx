2f3af7f6903243459ec60b63863b13e584921374
Antony <gitaumuangi@gmail.com>
2024-09-27 13:03:23 +0300

    Add a text input

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,6 +18,7 @@ export default function Layout() {
         name="counter"
         options={{
           title: "Counter",
+          headerShown: false,
           tabBarIcon: ({ color, size }) => (
             <AntDesign name="clockcircleo" size={size} color={color} />
           ),
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,47 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
 import ShoppingListItem from "../components/ShoppingListItem";
 import { theme } from "../theme";
+import { useState } from "react";
 
+type ShoppingListItemType = {
+  id: string,
+  name: string,
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem title="Coffee" />
-      <ShoppingListItem title="Tea" isCompleted={true} />
-      <ShoppingListItem title="Milk" isCompleted={true} />
+      <TextInput
+        style={styles.textInput}
+        placeholder="E.g Coffee"
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="previous"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem title={item.name} key={item.id} />
+      ))}
     </View>
   );
 }
@@ -16,6 +50,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    borderRadius: 50,
+    fontSize: 18,
   },
 });
